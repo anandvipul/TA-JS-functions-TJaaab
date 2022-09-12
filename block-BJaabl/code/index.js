3cbe39b54062004e0efe03f2abf83beeca099afa
Vipul Anand <anandvipul01@gmail.com>
2022-09-12 18:36:15 +0530

    Assignment: Argument and Pattern

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -10,6 +10,9 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -33,6 +36,40 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(num1, num2, operation) {
+  if (
+    typeof num1 == "number" &&
+    typeof num2 == "number" &&
+    (operation == "add" ||
+      operation == "sub" ||
+      operation == "mul" ||
+      operation == "div")
+  ) {
+    switch (operation) {
+      case "add":
+        return num1 + num2;
+        break;
+
+      case "sub":
+        return num1 - num2;
+        break;
+
+      case "mul":
+        return num1 * num2;
+        break;
+
+      case "div":
+        return num1 / num2;
+        break;
+
+      default:
+        alert("Enter A valid Operation");
+        break;
+    }
+  } else {
+    alert("Enter Valid Input");
+  }
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +79,29 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(year) {
+  if (year % 100 == 0) {
+    if (year % 400 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(num) {
+  if (num == 1) {
+    return 1;
+  } else {
+    return num * getFactorial(num - 1);
+  }
+}
